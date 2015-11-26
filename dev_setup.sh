c10d95c1de184e9651876e12684378074333a7c4
Sebastian Osiński <seb.osinski@gmail.com>
2015-11-26 10:17:44 +0100

    Add new tap - caskroom unofficial
    Needed for JavaFX Scene Builder
    (the cask was moved from official repository to unofficial)

--- dev_setup.sh
+++ dev_setup.sh
@@ -6,6 +6,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 echo "Installing Cask..."
 brew install caskroom/cask/brew-cask
 brew cask tap caskroom/versions
+brew cask tap caskroom/unofficial
 
 #install packs
 cd packs
