6aefb51c6229c78733267c585ac54850c92424c9
Sebastian Osiński <seb.osinski@gmail.com>
2016-06-11 22:09:16 +0200

    Remove scene builder cask

--- packs/java_dev_pack.sh
+++ packs/java_dev_pack.sh
@@ -10,6 +10,3 @@ brew install gradle
 echo "Installing IntelliJ IDEA..."
 #brew cask install intellij-idea-ce #community version - free one
 brew cask install intellij-idea #ultimate version - paid one
-
-echo "Installing JavaFX Scene Builder..."
-brew cask install javafx-scene-builder
